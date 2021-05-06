208f727cbd9cdbbf83c4f90b3ba4467c05e6ec5c
Filip Smets <filipsmetsbelgium@gmail.com>
2021-05-06 20:15:38 +0200

    Add database

---
+++ server/database/database.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS `plugins` (
+    id INT UNSIGNED AUTO_INCREMENT,
+    name VARCHAR(255),
+    creation datetime DEFAULT CURRENT_TIMESTAMP,
+    updatedatetime datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    description TEXT,
+    PRIMARY KEY(id),
+    INDEX(name)
+);
+
+CREATE TABLE IF NOT EXISTS 'versions' (
+    pluginid INT UNSIGNED AUTO_INCREMENT,
+    major TINYINT UNSIGNED,
+    minor TINYINT UNSIGNED,
+    patch TINYINT UNSIGNED,
+    releasenotes description TEXT,
+    creation datetime DEFAULT CURRENT_TIMESTAMP,
+    updatedatetime datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    INDEX(pluginid),
+    INDEX(updatedatetime)
+);
