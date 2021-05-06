d519e971960f8bb8302d108451ff545b70507073
Filip Smets <filipsmetsbelgium@gmail.com>
2021-05-06 21:58:42 +0200

    Add req and res to new plugin handler

--- server/routes/plugin/new.js
+++ server/routes/plugin/new.js
@@ -8,7 +8,7 @@ module.exports = (database) => {
     router.post('/', [
         body('name').isLength({ min: 3, max: 255 }),
         body('description')
-    ], async () => {
+    ], async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
