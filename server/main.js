6dba417cb0fd79d7d3bc48f0570ec795d9b7ec2f
Filip Smets <filipsmetsbelgium@gmail.com>
2021-05-06 21:35:19 +0200

    Add uuid library + initial new plugin endpoint

--- server/main.js
+++ server/main.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const app = express()
-const database = {};
 
-app.use('/plugin', require('./routes/plugin/_')(database));
+var mysql = require('mysql');
+var pool  = mysql.createPool({
+  connectionLimit : 10,
+  host            : 'example.org',
+  user            : 'bob',
+  password        : 'secret',
+  database        : 'my_db'
+});
+
+app.use(express.json());
+
+app.use('/plugin', require('./routes/plugin/_')(pool));
 
 app.listen(8765, '0.0.0.0', () => {
     console.log("Listening...")
--- server/routes/plugin/new.js
+++ server/routes/plugin/new.js
@@ -1,9 +1,27 @@
 const router = require('express').Router({ mergeParams: true });
-//const { body } = require('express-validator');
+const { body, validationResult } = require('express-validator');
+const { v4: uuidv4 } = require('uuid');
 
-module.exports = () => {
-    router.get('/', async () => {
-        console.log("it works");
+const INSERT_QUERY = "INSERT INTO `plugins` (uuid, useruuid, name, description) VALUES (UUID_TO_BIN(?), UUID_TO_BIN(?), ?, ?)";
+
+module.exports = (database) => {
+    router.post('/', [
+        body('name').isLength({ min: 3, max: 255 }),
+        body('description')
+    ], async () => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        // TODO userid from jwt token
+        const pluginuuid = uuidv4();
+        const useruuid = uuidv4();
+        database.query(INSERT_QUERY, [pluginuuid, useruuid, req.body.name, req.body.description], (err, result) => {
+            return res.status(200).json({ 
+                uuid: pluginuuid
+             });
+        });
     });
 
     return router;
