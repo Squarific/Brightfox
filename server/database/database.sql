f9a0650d6044a7e8879fcea6233ef3826b22f74f
Filip Smets <filipsmetsbelgium@gmail.com>
2021-05-06 20:55:43 +0200

    Add user uuid and use uuid for plugins

--- server/database/database.sql
+++ server/database/database.sql
@@ -1,21 +1,27 @@
 CREATE TABLE IF NOT EXISTS `plugins` (
-    id INT UNSIGNED AUTO_INCREMENT,
+    uuid VARCHAR(36),
     name VARCHAR(255),
+    useruuid VARCHAR(36),
     creation datetime DEFAULT CURRENT_TIMESTAMP,
     updatedatetime datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     description TEXT,
-    PRIMARY KEY(id),
-    INDEX(name)
+    PRIMARY KEY(uuid),
+    INDEX(name),
+    INDEX(useruuid),
+    INDEX(creation),
+    INDEX(updatedatetime)
 );
 
 CREATE TABLE IF NOT EXISTS `versions` (
-    pluginid INT UNSIGNED AUTO_INCREMENT,
+    pluginuuid VARCHAR(36),
     major TINYINT UNSIGNED,
     minor TINYINT UNSIGNED,
     patch TINYINT UNSIGNED,
     releasenotes TEXT,
     creation datetime DEFAULT CURRENT_TIMESTAMP,
     updatedatetime datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    INDEX(pluginid),
-    INDEX(updatedatetime)
+    INDEX(pluginuuid),
+    INDEX(updatedatetime),
+    INDEX(major, minor, patch),
+    INDEX(creation)
 );
