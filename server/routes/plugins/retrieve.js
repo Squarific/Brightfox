ecddc69f3c1c6c970a9a406f2a87ff11bd7cabb9
Filip Smets <filipsmetsbelgium@gmail.com>
2021-05-07 21:09:21 +0200

    Fix retrieve endpoint

--- server/routes/plugins/retrieve.js
+++ server/routes/plugins/retrieve.js
@@ -2,7 +2,7 @@ const router = require('express').Router({ mergeParams: true });
 const { body, validationResult } = require('express-validator');
 const { v4: uuidv4 } = require('uuid');
 
-const SELECT_QUERY = "SELECT name, description from  `plugins` WHERE uuid = ?";
+const SELECT_QUERY = "SELECT name, description from  `plugins` WHERE uuid = UUID_TO_BIN(?)";
 const GENERIC_DB_ERROR = {
     errors: [{
         msg: "Internal database error"
@@ -11,7 +11,7 @@ const GENERIC_DB_ERROR = {
 
 module.exports = (database) => {
     router.post('/', [
-        body('pluginuuid').isLength({ min: 36, max: 36 })
+        body('uuid').isLength({ min: 36, max: 36 })
     ], async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
@@ -19,15 +19,15 @@ module.exports = (database) => {
         }
 
         // TODO userid from jwt token
-        database.query(UPDATE_QUERY, [req.body.name, req.body.description, req.body.uuid, req.body.useruuid], (err, result) => {
+        database.query(SELECT_QUERY, [req.body.uuid], (err, result) => {
             if (err) {
-                console.log("Retrieve plugin database error", err, pluginuuid, useruuid, req.body.name, req.body.description);
+                console.log("Retrieve plugin database error", err, req.body.uuid);
                 return res.status(504).json(GENERIC_DB_ERROR);
             }
 
             return res.status(200).json({
-                uuid: pluginuuid
-             });
+                plugin: result[0]
+            });
         });
     });
 
