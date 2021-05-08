5095e5da847c0cf1f329a7df31564156e0abab5a
Ilger Depraetere <ilgerdepraetere@gmail.com>
2021-05-08 15:31:07 +0200

    retrieve and update use uuid in url

--- server/routes/plugins/retrieve.js
+++ server/routes/plugins/retrieve.js
@@ -1,8 +1,8 @@
 const router = require('express').Router({ mergeParams: true });
-const { body, validationResult } = require('express-validator');
-const { v4: uuidv4 } = require('uuid');
+const { param, validationResult } = require('express-validator');
+// const { v4: uuidv4 } = require('uuid');
 
-const SELECT_QUERY = "SELECT BIN_TO_UUID(useruuid), name, description, creation, updatedatetime from  `plugins` WHERE uuid = UUID_TO_BIN(?)";
+const SELECT_QUERY = "SELECT BIN_TO_UUID(uuid) as uuid, BIN_TO_UUID(useruuid) as useruuid, name, description, creation, updatedatetime from  `plugins` WHERE uuid = UUID_TO_BIN(?)";
 const GENERIC_DB_ERROR = {
     errors: [{
         msg: "Internal database error"
@@ -10,17 +10,17 @@ const GENERIC_DB_ERROR = {
 };
 
 module.exports = (database) => {
-    router.get('/', [
-        body('uuid').isLength({ min: 36, max: 36 })
+    router.get('/:uuid', [
+        param('uuid').isLength({ min: 36, max: 36 })
     ], async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
 
-        database.query(SELECT_QUERY, [req.body.uuid], (err, result) => {
+        database.query(SELECT_QUERY, [req.params.uuid], (err, result) => {
             if (err) {
-                console.log("Retrieve plugin database error", err, req.body.uuid);
+                console.log("Retrieve plugin database error", err, req.params.uuid);
                 return res.status(504).json(GENERIC_DB_ERROR);
             }
 
