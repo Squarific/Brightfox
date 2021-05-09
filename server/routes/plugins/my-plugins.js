a2934200b481173446c3df197ff262d9a65d47fe
Ilger Depraetere <ilgerdepraetere@gmail.com>
2021-05-09 19:34:54 +0200

    fix registering my-plugigns.js

--- server/routes/plugins/my-plugins.js
+++ server/routes/plugins/my-plugins.js
@@ -1,7 +1,7 @@
 const router = require('express').Router({ mergeParams: true });
 const { validationResult } = require('express-validator');
 
-const SELECT_QUERY = "SELECT BIN_TO_UUID(uuid) as uuid, BIN_TO_UUID(useruuid) as useruuid, name, description, creation, updatedatetime from  `plugins`";
+const SELECT_QUERY = "SELECT BIN_TO_UUID(uuid) as uuid, BIN_TO_UUID(useruuid) as useruuid, name, description, creation, updatedatetime from  `plugins` WHERE useruuid = UUID_TO_BIN(?)";
 const GENERIC_DB_ERROR = {
     errors: [{
         msg: "Internal database error"
@@ -16,7 +16,9 @@ module.exports = (database) => {
             return res.status(400).json({ errors: errors.array() });
         }
         // TODO filter on jwt
-        database.query(SELECT_QUERY, (err, result) => {
+        let useruuid = "20a7e88c-f224-4780-aa85-783bbe65a72b";
+
+        database.query(SELECT_QUERY, [useruuid], (err, result) => {
             if (err) {
                 console.log("Retrieve plugin database error", err);
                 return res.status(504).json(GENERIC_DB_ERROR);
