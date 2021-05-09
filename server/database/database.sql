cbe39ffc047a6faff8b84f28d373088cf14e8031
Ilger Depraetere <ilgerdepraetere@gmail.com>
2021-05-09 17:30:40 +0200

    database.sql all values not null

--- server/database/database.sql
+++ server/database/database.sql
@@ -1,17 +1,17 @@
-CREATE DATABASE `pluginmods`;
+CREATE DATABASE IF NOT EXISTS `pluginmods`;
 USE `pluginmods`;
 
 CREATE TABLE IF NOT EXISTS `plugins` (
-    uuid binary(16),
-    useruuid binary(16),
+    uuid binary(16) NOT NULL,
+    useruuid binary(16) NOT NULL,
 
-    name VARCHAR(255),
-    description TEXT,
+    name VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
 
-    verified BOOLEAN DEFAULT FALSE,
+    verified BOOLEAN DEFAULT FALSE NOT NULL,
 
-    creation datetime DEFAULT CURRENT_TIMESTAMP,
-    updatedatetime datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    creation datetime DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updatedatetime datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
 
     PRIMARY KEY(uuid),
     INDEX(name),
@@ -22,19 +22,19 @@ CREATE TABLE IF NOT EXISTS `plugins` (
 );
 
 CREATE TABLE IF NOT EXISTS `versions` (
-    pluginuuid binary(16),
+    pluginuuid binary(16) NOT NULL,
 
-    major TINYINT UNSIGNED,
-    minor TINYINT UNSIGNED,
-    patch TINYINT UNSIGNED,
+    major TINYINT UNSIGNED NOT NULL,
+    minor TINYINT UNSIGNED NOT NULL,
+    patch TINYINT UNSIGNED NOT NULL,
 
-    releasenotes TEXT,
-    source TEXT,
+    releasenotes TEXT NOT NULL,
+    source TEXT NOT NULL,
 
-    verified BOOLEAN DEFAULT FALSE,
+    verified BOOLEAN DEFAULT FALSE NOT NULL,
 
-    creation datetime DEFAULT CURRENT_TIMESTAMP,
-    updatedatetime datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,\
+    creation datetime DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updatedatetime datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
 
     UNIQUE(pluginuuid, major, minor, patch),
     INDEX(pluginuuid),
