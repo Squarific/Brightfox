18fd69a071a7d69f95e8f6587bb2085ae717c029
Ilger Depraetere <ilgerdepraetere@gmail.com>
2021-05-09 16:19:23 +0200

    add verifiction field to plugin and version

--- server/database/database.sql
+++ server/database/database.sql
@@ -8,27 +8,37 @@ CREATE TABLE IF NOT EXISTS `plugins` (
     name VARCHAR(255),
     description TEXT,
 
+    verified BOOLEAN DEFAULT FALSE;
+
     creation datetime DEFAULT CURRENT_TIMESTAMP,
     updatedatetime datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 
     PRIMARY KEY(uuid),
     INDEX(name),
     INDEX(useruuid),
+    INDEX(verified),
     INDEX(creation),
-    INDEX(updatedatetime)
+    INDEX(updatedatetime),
 );
 
 CREATE TABLE IF NOT EXISTS `versions` (
     pluginuuid binary(16),
+
     major TINYINT UNSIGNED,
     minor TINYINT UNSIGNED,
     patch TINYINT UNSIGNED,
+
     releasenotes TEXT,
     source TEXT,
+
+    verified BOOLEAN DEFAULT FALSE;
+
     creation datetime DEFAULT CURRENT_TIMESTAMP,
     updatedatetime datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
     INDEX(pluginuuid),
     INDEX(updatedatetime),
     INDEX(major, minor, patch),
+    INDEX(verified)
     INDEX(creation)
 );
